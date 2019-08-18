d124edd892aad97a85fab6a78f2714246d3b1f3b
unknown <51wanderer51@gmail.com>
2019-08-18 15:54:56 -0400

    save dark mode with cookie

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,6 +1,36 @@
 var dark = false;
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+var mode = getCookie("mode");
+if(mode == "dark") {
+	dark = true;
+	document.getElementById("pagestyle").setAttribute("href", "css/styleDark.css");
+}
+
 function switchTheme() {
-	if(!dark) document.getElementById("pagestyle").setAttribute("href", "css/styleDark.css");
-	else document.getElementById("pagestyle").setAttribute("href", "css/styleLight.css");
+	if(!dark) {
+		document.getElementById("pagestyle").setAttribute("href", "css/styleDark.css");
+		document.cookie = "mode=dark";
+	}
+	else {
+		document.getElementById("pagestyle").setAttribute("href", "css/styleLight.css");
+		document.cookie = "mode=light";
+	}
 	dark = !dark;
 }
